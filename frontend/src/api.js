02541e5c8afde1956685a251dc6deb4915be44b6
Eka3asmara <eka3asmarak@gmail.com>
2026-01-19 22:58:11 +0700

    Update API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,5 @@
-import axios from "axios";
-
 const api = axios.create({
-  baseURL: "https://nama-proyek-backend-anda.vercel.app/api", 
+  baseURL: "https://uas-resto-eka.vercel.app/api", 
 });
 
 export default api;
